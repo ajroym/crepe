5bb0ae0b6ab35efe970f2d1b0506608db9ec5922
AJ <ajroym00@gmail.com>
2025-08-27 18:02:27 -0400

    Configured app for Okta login/logout with placeholder values

--- src/main/java/com/majr/crepe/config/SecurityConfiguration.java
+++ src/main/java/com/majr/crepe/config/SecurityConfiguration.java
@@ -1,2 +1,59 @@
-package com.majr.crepe.config;public class SecurityConfiguration {
+package com.majr.crepe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers("/")
+                                                .permitAll()
+                                                .anyRequest()
+                                                .authenticated()
+        ).oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.addLogoutHandler(logoutHandler()));
+
+        return http.build();
+    }
+
+    private LogoutHandler logoutHandler() {
+        return ((request, response, authentication) -> {
+            try {
+                String baseUrl = "";
+                response.sendRedirect(baseUrl);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
 }
